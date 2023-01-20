ac8201e07c884a3cff831d382277fac1a0d69afe
jujube34 <418926101@qq.com>
2023-01-20 16:20:49 +0800

    Add files via upload

---
+++ Batch_process_of_multiple_imputation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# This is the code used for parallel multiple imputation
+PID=()
+for i in `seq 1 10` # create 10 threads of same R program
+do
+RNDseed=$RANDOM # get random number for the seed of multiple imputation algorithm
+	echo $RNDseed
+	Rscript Rscript_MI.R $RNDseed $1 & # input 2 arguments: random seed and k (the offset) for sensitivity analysis
+	PID[i]=$!
+done
+
+# Ensure that every threads created return their results
+# before closing of the current program
+for i in `seq 0 9`
+do
+	echo wait
+	echo ${PID[i]}
+        wait ${PID[i]}
+done
+
